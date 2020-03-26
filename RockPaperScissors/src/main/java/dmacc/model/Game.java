35d7ca1e48fe151e103020d3cf6c8bfc79e61667
kjkleindorfer <kjkleindorfer@dmacc.edu>
2020-03-26 09:59:21 -0500

    Two issues inside the setComputerPlayerToRandom( ) method
    See the comments inside the method

--- RockPaperScissors/src/main/java/dmacc/model/Game.java
+++ RockPaperScissors/src/main/java/dmacc/model/Game.java
@@ -78,11 +78,28 @@ public class Game {
 	private void setComputerPlayerToRandom() {
 		// TODO Auto-generated method stub
 
-		String computerPlayer = "";
+		/*
+		 * String computerPlayer = ""; 
+		 * 
+		 * This is not using the instance variable - this is
+		 * creating a local variable inside the method called computerPlayer.
+		 * How do you reference and set the instance variable?
+		 * 
+		 * Hint: by commenting out the declaration of the variable like I have already
+		 * done. I would recommend changing all the computerPlayer = "Rock"; sets to
+		 * this.computerPlayer = "Rock"; so you really understand that the instance
+		 * variable is being set
+		 */
 
-		Random theChoice = new Random();
-
-		int ComputerPlayerToRandom = theChoice.nextInt(3);
+		/*
+		 * Random theChoice = new Random();
+		 * 
+		 * int ComputerPlayerToRandom = theChoice.nextInt(3);
+		 * 
+		 * This is duplicate code to below.  Except you are using the int i variable and not
+		 * the ComputerPlayerToRandom variable.
+		 */
+		
 
 		Random r = new Random();
 
@@ -100,15 +117,26 @@ public class Game {
 
 			computerPlayer = "Scissors";
 
-		this.setComputerPlayerToRandom();
-
+		/*
+		 * this.setComputerPlayerToRandom(); 
+		 * 
+		 * why are you calling this method again
+		 * inside this method? You've basically made a recursive function.
+		 * This is why the 500 error is appearing over and over again.
+		 */
+		
+		/*
+		 * After making these changes, you'll still have some errors in the results.html
+		 * to resolve but it will work.
+		 */		
 	}
 
-	public String getWinner() {
-		return winner;
-	}
-
-	public void setWinner(String winner) {
-		this.winner = winner;
-	}
+	/*
+	 * public String getWinner() { return winner; }
+	 * 
+	 * public void setWinner(String winner) { this.winner = winner; }
+	 * 
+	 * You don't need these methods because the @Data annotation 
+	 * automatically generates these for you.
+	 */
 }
